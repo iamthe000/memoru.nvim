89be5841fb4bd44f2738058bc36e4446bb39ff30
minimium <umekichi3hoshi@gmail.com>
2026-04-19 19:35:09 +0900

    Create my_memo.lua

---
+++ lua/my_memo/plugin/my_memo.lua
@@ -0,0 +1,11 @@
+-- plugin/my_memo.lua
+
+vim.api.nvim_create_user_command('Memo', function()
+  require('my_memo').toggle()
+end, {})
+
+vim.keymap.set('n', '<leader>memo', '<cmd>Memo<CR>', { 
+  noremap = true, 
+  silent = true, 
+  desc = "メモウィンドウの切り替え" 
+})
